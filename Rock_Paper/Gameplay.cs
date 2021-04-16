170c368f3b800b1bae05558b93725a8501a7f76d
Elena Matusova <matusova.yelena@gmail.com>
2021-04-16 10:32:28 +0300

    Adding

---
+++ Rock_Paper/Gameplay.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper
+{
+    class Gameplay
+    {
+        private List<string> _allSteps;
+        private int _stepsCount;
+        private readonly byte[] _random = new byte[16];
+
+        private int _computerStep;
+        private int _userStep;
+        private int _avarage;
+
+        public Gameplay(List<string> allSteps)
+        {
+            _allSteps = allSteps;
+            _stepsCount = _allSteps.Count;
+            _avarage = allSteps.Count / 2;
+
+        }
+
+        private string BitConvert(byte[] bc)
+        {
+            return BitConverter.ToString(bc).Replace("-", string.Empty);
+        }
+
+        private string GetHmac()
+        {
+            var hmacSha256 = new HMACSHA256(_random);
+            var myHmac = hmacSha256.ComputeHash(Encoding.UTF8.GetBytes(_computerStep.ToString()));
+            return BitConvert(myHmac);
+        }
+
+        private void ComputerStep()
+        {
+            _computerStep = RandomNumberGenerator.GetInt32(0,_allSteps.Count);
+            RandomNumberGenerator.Fill(_random);
+            
+        }
+
+        private void userStep()
+        {
+            while (true)
+            {
+                var input = int.Parse(Console.ReadLine());
+                try
+                {
+                    if (input > _stepsCount || input < 0) throw new ArgumentOutOfRangeException();
+                    _userStep = input;
+                    return;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input! Try again!");
+                    ShowMenu();
+                }
+            }
+
+        }
+
+        private void ShowMenu()
+        {
+            Console.WriteLine("Available moves:");
+            for (int i = 0; i < _allSteps.Count; i++)
+            {
+                Console.WriteLine($"{i + 1} - {_allSteps[i]}");
+            }
+            Console.WriteLine("0 - Exit");
+            Console.Write("Enter your move: ");
+        }
+
+        internal void Start()
+        {
+            ComputerStep();
+            Console.WriteLine($"HMAC: {GetHmac()}");
+            ShowMenu();
+            userStep();
+            if (_userStep == 0)
+            {
+                Console.WriteLine("Thank you for the game! See you!");
+                return;
+            }
+
+            Console.WriteLine($"Your move: {_allSteps[_userStep - 1]}");
+            Console.WriteLine($"Computer move: {_allSteps[_computerStep]}");
+            ChoiceResult();
+            
+            Console.WriteLine($"HMAC key: {BitConvert(_random)}");
+        }
+
+        private void ChoiceResult()
+        {
+            _userStep -= 1;
+            // var r = input - _avarage;
+            var result = (_computerStep - _userStep + _avarage) % _allSteps.Count;
+            if (result > _avarage)
+            {
+                Console.WriteLine("You lose!");
+            }
+            else if (result < _avarage)
+            {
+               Console.WriteLine("You win!");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+            }
+            
+        } 
+    }
+}
---
+++ Rock_Paper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Rock_Paper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            { Console.WriteLine("No elements to play!"); return; }
+            else if (args.Length % 2 == 0)
+            { Console.WriteLine("Number of elements to play must be odd!"); return; }
+            else if (args.Length < 3)
+            { Console.WriteLine("Number of elements should be at least 3!"); return; }
+            else if (args.Length != args.Distinct().Count())
+            { Console.WriteLine("Elements to play should be unique"); return; }
+            else { var gameStart = new Gameplay(args.ToList()); gameStart.Start(); }
+
+            
+        
+     
+            Console.ReadKey();
+        }
+    }
+}
